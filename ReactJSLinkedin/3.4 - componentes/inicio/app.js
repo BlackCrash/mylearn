6a1ad9bb859400bd78f212868faeaa42652be1f5
Eduardo Contreras <Eduardo.Contreras@latam.com>
2018-06-18 23:44:04 -0300

    example of run

---
+++ ReactJSLinkedin/3.4 - componentes/inicio/app.js
@@ -0,0 +1,8 @@
+const HelloMessage = React.createClass({
+    render: function() {
+        return <div>Hello {this.props.name}</div>;
+    }
+ });
+
+ ReactDOM.render(<HelloMessage name="John" />, 
+     document.getElementById('mensaje'));
