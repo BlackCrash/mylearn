63f09377bf32b6df65c187ffb65fdf29411f4dc6
Eduardo Contreras <Eduardo.Contreras@latam.com>
2018-06-22 20:48:49 -0300

    apuntes...

--- ReactJSLinkedin/3.4 - componentes/inicio/app.js
+++ ReactJSLinkedin/3.4 - componentes/inicio/app.js
@@ -1,8 +1,26 @@
-const HelloMessage = React.createClass({
-    render: function() {
-        return <div>Hello {this.props.name}</div>;
-    }
- });
 
- ReactDOM.render(<HelloMessage name="John" />, 
-     document.getElementById('mensaje'));
+/* Para crear componentes como clase de React a constante. */
+/* const HelloMessage = React.createClass({
+   render: function() {
+       return <div>Hello {this.props.name}</div>;
+  }
+}); */
+
+/* para crear componentes directamente. */
+class HelloMessage extends React.Component {
+   render() {
+     return <h1>Hello, {this.props.name}</h1>;
+   }
+};
+
+class App extends React.Component {
+    render() {
+        return <fieldset>
+            <HelloMessage name="Eduardo" />
+            <HelloMessage name="Johana" />
+        </fieldset>
+    }
+};
+
+ReactDOM.render(<div><App /></div>, 
+    document.getElementById('mensaje'));
